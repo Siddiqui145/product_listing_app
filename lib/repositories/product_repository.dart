db6a4163a57ee370b9f54cad04196f57b7151d62
Siddiqui Taha <tahasiddiqui541@gmail.com>
2025-02-14 19:34:25 +0530

    fetching data properly

--- lib/repositories/product_repository.dart
+++ lib/repositories/product_repository.dart
@@ -1,12 +1,14 @@
-import 'package:flutter/services.dart';
-import '../models/product.dart';
-
+import 'dart:convert';
+import 'package:assignment/models/product.dart';
+import 'package:http/http.dart' as http;
 class ProductRepository {
   Future<List<Product>> fetchProducts() async {
-    try {
-      final response = await rootBundle.loadString('assets/data.json');
-      return Product.fromJsonList(response);
-    } catch (e) {
+    final url = Uri.parse('https://example.com/data.json'); 
+    final response = await http.get(url);
+    if (response.statusCode == 200) {
+      final data = json.decode(response.body)['products'] as List;
+      return data.map((json) => Product.fromJson(json)).toList();
+    } else {
       throw Exception('Failed to load products');
     }
   }
