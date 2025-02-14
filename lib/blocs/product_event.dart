7528147389f5cb261dd2794a7837fa9821bc5db8
Siddiqui Taha <tahasiddiqui541@gmail.com>
2025-02-14 19:36:29 +0530

    defined event & state for modifications

--- lib/blocs/product_event.dart
+++ lib/blocs/product_event.dart
@@ -1,17 +1,8 @@
-import 'package:equatable/equatable.dart';
-
-abstract class ProductEvent extends Equatable {
-  @override
-  List<Object> get props => [];
-}
+abstract class ProductEvent {}
 
 class LoadProducts extends ProductEvent {}
 
 class SearchProducts extends ProductEvent {
   final String query;
-
   SearchProducts(this.query);
-
-  @override
-  List<Object> get props => [query];
 }
--- lib/blocs/product_state.dart
+++ lib/blocs/product_state.dart
@@ -1,29 +1,16 @@
-import 'package:equatable/equatable.dart';
-import '../../models/product.dart';
 
-abstract class ProductState extends Equatable {
-  @override
-  List<Object> get props => [];
-}
+import 'package:assignment/models/product.dart';
 
-class ProductInitial extends ProductState {}
+abstract class ProductState {}
 
 class ProductLoading extends ProductState {}
 
 class ProductLoaded extends ProductState {
   final List<Product> products;
-
   ProductLoaded(this.products);
-
-  @override
-  List<Object> get props => [products];
 }
 
 class ProductError extends ProductState {
   final String message;
-
   ProductError(this.message);
-
-  @override
-  List<Object> get props => [message];
 }
