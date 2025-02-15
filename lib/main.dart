bf8d7fb6506069e6e1288b1b3f89765957966f2e
Siddiqui Taha <tahasiddiqui541@gmail.com>
2025-02-15 23:28:45 +0530

    fixes & imporvements

--- lib/main.dart
+++ lib/main.dart
@@ -24,6 +24,7 @@ class MyApp extends StatelessWidget {
       title: 'Product Listing App',
       theme: ThemeData(primarySwatch: Colors.blue),
       home: const HomeScreen(),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
